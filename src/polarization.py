7edfc8f93a192b8ac7a88b89b7dad803b9e9c99f
jonasq99 <104920844+jonasq99@users.noreply.github.com>
2024-08-08 00:10:57 +0200

    chore: Update graph loader import statements and remove unused code

--- src/polarization.py
+++ src/polarization.py
@@ -1,13 +1,32 @@
 import heapq
 import itertools
 from multiprocessing import Pool, cpu_count
+from typing import Dict, List, Set, Tuple
 
 import networkx as nx
 import numpy as np
 from tqdm import tqdm
 
 
-def f_vectorized(v, F_set, adj_matrix, opposite_color_nodes):
+def f_vectorized(
+    v: int,
+    F_set: Set[Tuple[int, int]],
+    adj_matrix: np.ndarray,
+    opposite_color_nodes: Dict[int, List[int]],
+) -> float:
+    """
+    Calculate the activation probability for a node v given a set of added edges.
+
+    Parameters:
+    v (int): The node for which to calculate the activation probability.
+    F_set (Set[Tuple[int, int]]): The set of added edges.
+    adj_matrix (np.ndarray): The adjacency matrix of the graph.
+    opposite_color_nodes (Dict[int, List[int]]): A dictionary mapping each node
+    to its opposite color nodes.
+
+    Returns:
+    float: The activation probability for the node v.
+    """
     if len(opposite_color_nodes[v]) == 0:
         return 0
 
@@ -17,26 +36,96 @@ def f_vectorized(v, F_set, adj_matrix, opposite_color_nodes):
     return activated_count / len(opposite_color_nodes[v])
 
 
-def tau_vectorized(R, F_set, adj_matrix, opposite_color_nodes):
+def tau_vectorized(
+    R: List[int],
+    F_set: Set[Tuple[int, int]],
+    adj_matrix: np.ndarray,
+    opposite_color_nodes: Dict[int, List[int]],
+) -> float:
+    """
+    Calculate the average activation probability for a set of nodes R.
+
+    Parameters:
+    R (List[int]): The set of nodes for which to calculate the average activation probability.
+    F_set (Set[Tuple[int, int]]): The set of added edges.
+    adj_matrix (np.ndarray): The adjacency matrix of the graph.
+    opposite_color_nodes (Dict[int, List[int]]): A dictionary mapping each node to
+    its opposite color nodes.
+
+    Returns:
+    float: The average activation probability for the set of nodes R.
+    """
     f_values = np.array(
         [f_vectorized(v, F_set, adj_matrix, opposite_color_nodes) for v in R]
     )
     return np.mean(f_values)
 
 
-def edge_impact(edge, C, F_set, adj_matrix, opposite_color_nodes):
+def edge_impact(
+    edge: Tuple[int, int],
+    C: List[int],
+    F_set: Set[Tuple[int, int]],
+    adj_matrix: np.ndarray,
+    opposite_color_nodes: Dict[int, List[int]],
+) -> float:
+    """
+    Calculate the impact of adding a specific edge on the average activation probability.
+
+    Parameters:
+    edge (Tuple[int, int]): The edge to add.
+    C (List[int]): The set of nodes of a given color.
+    F_set (Set[Tuple[int, int]]): The current set of added edges.
+    adj_matrix (np.ndarray): The adjacency matrix of the graph.
+    opposite_color_nodes (Dict[int, List[int]]): A dictionary mapping each
+    node to its opposite color nodes.
+
+    Returns:
+    float: The impact of adding the edge.
+    """
     F_set.add(edge)
     impact = tau_vectorized(C, F_set, adj_matrix, opposite_color_nodes)
     F_set.remove(edge)
     return impact
 
 
-def compute_initial_impact(edge, C, adj_matrix, opposite_color_nodes):
+def compute_initial_impact(
+    edge: Tuple[int, int],
+    C: List[int],
+    adj_matrix: np.ndarray,
+    opposite_color_nodes: Dict[int, List[int]],
+) -> Tuple[float, Tuple[int, int]]:
+    """
+    Compute the initial impact of an edge without any pre-existing edges in the set.
+
+    Parameters:
+    edge (Tuple[int, int]): The edge to evaluate.
+    C (List[int]): The set of nodes of a given color.
+    adj_matrix (np.ndarray): The adjacency matrix of the graph.
+    opposite_color_nodes (Dict[int, List[int]]): A dictionary mapping each
+    node to its opposite color nodes.
+
+    Returns:
+    Tuple[float, Tuple[int, int]]: A tuple containing the negative impact and the edge.
+    """
     impact = edge_impact(edge, C, set(), adj_matrix, opposite_color_nodes)
     return -impact, edge
 
 
-def get_candidate_edges(G, red_nodes, blue_nodes, perc_k=5):
+def get_candidate_edges(
+    G: nx.Graph, red_nodes: List[int], blue_nodes: List[int], perc_k: int = 5
+) -> Set[Tuple[int, int]]:
+    """
+    Get candidate edges based on top-k nodes by degree from red and blue nodes.
+
+    Parameters:
+    G (nx.Graph): The input graph.
+    red_nodes (List[int]): List of red nodes.
+    blue_nodes (List[int]): List of blue nodes.
+    perc_k (int): Percentage of top-k nodes to consider. Defaults to 5.
+
+    Returns:
+    Set[Tuple[int, int]]: Set of candidate edges.
+    """
     red_topk = get_topk_nodes(G, red_nodes, perc_k)
     blue_topk = get_topk_nodes(G, blue_nodes, perc_k)
     candidate_edges = list(itertools.product(red_topk, blue_topk))
@@ -44,14 +133,47 @@ def get_candidate_edges(G, red_nodes, blue_nodes, perc_k=5):
     return set(candidate_edges)
 
 
-def get_topk_nodes(G, nodes, perc_k=5):
+def get_topk_nodes(G: nx.Graph, nodes: List[int], perc_k: int = 5) -> List[int]:
+    """
+    Get the top-k nodes by degree from a list of nodes.
+
+    Parameters:
+    G (nx.Graph): The input graph.
+    nodes (List[int]): List of nodes to evaluate.
+    perc_k (int): Percentage of top-k nodes to consider. Defaults to 5.
+
+    Returns:
+    List[int]: List of top-k nodes by degree.
+    """
     degrees = {i: G.degree(i) for i in nodes}
     k = int(len(degrees) / 100 * perc_k)
     topk = [i for i, _ in sorted(degrees.items(), key=lambda x: x[1], reverse=True)][:k]
     return topk
 
 
-def optimize_tau(C, G, k, red_nodes, blue_nodes, perc_k=5):
+def optimize_tau(
+    C: List[int],
+    G: nx.Graph,
+    k: int,
+    red_nodes: List[int],
+    blue_nodes: List[int],
+    perc_k: int = 5,
+) -> Tuple[Set[Tuple[int, int]], float]:
+    """
+    Optimize the average activation probability by adding up to k edges to the graph.
+
+    Parameters:
+    C (List[int]): The set of nodes of a given color.
+    G (nx.Graph): The input graph.
+    k (int): The maximum number of edges to add.
+    red_nodes (List[int]): List of red nodes.
+    blue_nodes (List[int]): List of blue nodes.
+    perc_k (int): Percentage of top-k nodes to consider for candidate edges. Defaults to 5.
+
+    Returns:
+    Tuple[Set[Tuple[int, int]], float]: A tuple containing the set of added edges and the best
+    average activation probability.
+    """
     candidate_edges = get_candidate_edges(G, red_nodes, blue_nodes, perc_k)
     heap = []
     adj_matrix = nx.to_numpy_array(G)
