82e67902115b9f6b384162e06e1c3879da80bf41
jonasq99 <104920844+jonasq99@users.noreply.github.com>
2024-08-28 17:09:03 +0200

    Optimiere Methoden zum Hinzufügen von Kanten und verbessere Code-Struktur

--- backup_code.py
+++ backup_code.py
@@ -14,3 +14,66 @@
                     round(modified_results_dict["polarization_scores"]["edge_based_polarization"], 3),
                     round(modified_results_dict["polarization_scores"]["modularity_based_polarization"], 3),
                     round(modified_results_dict["polarization_scores"]["homophily_based_polarization"], 3),"""
+
+
+def edge_based_polarization_score(G):
+    """
+    Calculate the polarization score based on the proportion of edges between different color groups.
+
+    A higher score indicates higher polarization, meaning more edges connect nodes from different groups.
+    """
+    E_total = G.number_of_edges()
+    E_diff = sum(1 for u, v in G.edges if G.nodes[u]["color"] != G.nodes[v]["color"])
+    polarization_score = E_diff / E_total if E_total > 0 else 0
+    return polarization_score
+
+
+def modularity_based_polarization_score(G):
+    """
+    Calculate the modularity-based polarization score.
+
+    A higher score indicates lower polarization, meaning most edges are within the same group (same color),
+    suggesting strong community structure. A lower or negative score indicates higher polarization.
+    """
+    partition = {
+        node: 0 if G.nodes[node]["color"] == "color1" else 1 for node in G.nodes
+    }
+    modularity_score = nx.algorithms.community.modularity(
+        G,
+        [
+            list(filter(lambda x: partition[x] == 0, partition)),
+            list(filter(lambda x: partition[x] == 1, partition)),
+        ],
+    )
+    return modularity_score
+
+
+def homophily_based_polarization_score(G):
+    """
+    Calculate the polarization score based on the proportion of neighbors within the same group.
+
+    A higher score indicates higher polarization, meaning there is less local cohesion within groups.
+    """
+    homophily_scores = []
+    for node in G.nodes:
+        neighbors = list(G.neighbors(node))
+        if neighbors:
+            same_color_neighbors = sum(
+                1
+                for neighbor in neighbors
+                if G.nodes[neighbor]["color"] == G.nodes[node]["color"]
+            )
+            homophily_scores.append(same_color_neighbors / len(neighbors))
+    polarization_score = 1 - np.mean(homophily_scores)
+    return polarization_score
+
+    """# Calculate polarization scores after the simulations
+    edge_polarization = edge_based_polarization_score(G)
+    modularity_polarization = modularity_based_polarization_score(G)
+    homophily_polarization = homophily_based_polarization_score(G)
+    
+    "polarization_scores": {
+            "edge_based_polarization": edge_polarization,
+            "modularity_based_polarization": modularity_polarization,
+            "homophily_based_polarization": homophily_polarization,
+        },"""
--- icm_diffusion.py
+++ icm_diffusion.py
@@ -1,4 +1,5 @@
 import concurrent.futures
+from collections import defaultdict
 
 import networkx as nx
 import numpy as np
@@ -40,7 +41,14 @@ def independent_cascade_model(G, seeds):
 
     total_activated_nodes = sum(1 for status in nodes_status.values() if status == 2)
 
-    return total_activated_nodes, color_activation_count
+    # Use defaultdict for counting
+    color_activated_nodes = defaultdict(int)
+    # Filter nodes with status == 2 and count their colors
+    for node, status in nodes_status.items():
+        if status == 2:
+            color_activated_nodes[G.nodes[node]["color"]] += 1
+
+    return total_activated_nodes, color_activation_count, color_activated_nodes
 
 
 def simulate_diffusion_single_run(G, seeds):
@@ -48,74 +56,27 @@ def simulate_diffusion_single_run(G, seeds):
     return independent_cascade_model(G, seeds)
 
 
-def edge_based_polarization_score(G):
-    """
-    Calculate the polarization score based on the proportion of edges between different color groups.
-
-    A higher score indicates higher polarization, meaning more edges connect nodes from different groups.
-    """
-    E_total = G.number_of_edges()
-    E_diff = sum(1 for u, v in G.edges if G.nodes[u]["color"] != G.nodes[v]["color"])
-    polarization_score = E_diff / E_total if E_total > 0 else 0
-    return polarization_score
-
-
-def modularity_based_polarization_score(G):
-    """
-    Calculate the modularity-based polarization score.
-
-    A higher score indicates lower polarization, meaning most edges are within the same group (same color),
-    suggesting strong community structure. A lower or negative score indicates higher polarization.
-    """
-    partition = {
-        node: 0 if G.nodes[node]["color"] == "color1" else 1 for node in G.nodes
-    }
-    modularity_score = nx.algorithms.community.modularity(
-        G,
-        [
-            list(filter(lambda x: partition[x] == 0, partition)),
-            list(filter(lambda x: partition[x] == 1, partition)),
-        ],
-    )
-    return modularity_score
-
-
-def homophily_based_polarization_score(G):
-    """
-    Calculate the polarization score based on the proportion of neighbors within the same group.
-
-    A higher score indicates higher polarization, meaning there is less local cohesion within groups.
-    """
-    homophily_scores = []
-    for node in G.nodes:
-        neighbors = list(G.neighbors(node))
-        if neighbors:
-            same_color_neighbors = sum(
-                1
-                for neighbor in neighbors
-                if G.nodes[neighbor]["color"] == G.nodes[node]["color"]
-            )
-            homophily_scores.append(same_color_neighbors / len(neighbors))
-    polarization_score = 1 - np.mean(homophily_scores)
-    return polarization_score
-
-
 def simulate_diffusion_ICM(G, seeds, num_simulations):
     """Simulate diffusion using the Independent Cascade model with parallel execution."""
     activated_nodes_list = []
     color_activation_counts = []
+    color_activated_nodes_list = []
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = [
             executor.submit(simulate_diffusion_single_run, G, seeds)
             for _ in range(num_simulations)
         ]
-        for future in tqdm(
+        """for future in tqdm(
             concurrent.futures.as_completed(futures), total=num_simulations
-        ):
-            activated_nodes, color_activation_count = future.result()
+        ):"""
+        for future in concurrent.futures.as_completed(futures):
+            activated_nodes, color_activation_count, color_activated_nodes = (
+                future.result()
+            )
             activated_nodes_list.append(activated_nodes)
             color_activation_counts.append(color_activation_count)
+            color_activated_nodes_list.append(color_activated_nodes)
 
     avg_activated_nodes = np.mean(activated_nodes_list)
     std_dev_activated_nodes = np.std(activated_nodes_list)
@@ -123,16 +84,15 @@ def simulate_diffusion_ICM(G, seeds, num_simulations):
     avg_color_activation_count = np.mean(color_activation_counts)
     std_dev_color_activation_count = np.std(color_activation_counts)
 
-    """# Calculate polarization scores after the simulations
-    edge_polarization = edge_based_polarization_score(G)
-    modularity_polarization = modularity_based_polarization_score(G)
-    homophily_polarization = homophily_based_polarization_score(G)
-    
-    "polarization_scores": {
-            "edge_based_polarization": edge_polarization,
-            "modularity_based_polarization": modularity_polarization,
-            "homophily_based_polarization": homophily_polarization,
-        },"""
+    # Calculate average and std deviation for activated nodes by color
+    avg_color_activated_nodes = {
+        color: np.mean([counts[color] for counts in color_activated_nodes_list])
+        for color in color_activated_nodes_list[0]
+    }
+    std_dev_color_activated_nodes = {
+        color: np.std([counts[color] for counts in color_activated_nodes_list])
+        for color in color_activated_nodes_list[0]
+    }
 
     # Combine all results into a single dictionary
     results = {
@@ -140,6 +100,8 @@ def simulate_diffusion_ICM(G, seeds, num_simulations):
         "std_dev_activated_nodes": std_dev_activated_nodes,
         "avg_color_activation_count": avg_color_activation_count,
         "std_dev_color_activation_count": std_dev_color_activation_count,
+        "avg_color_activated_nodes": avg_color_activated_nodes,
+        "std_dev_color_activated_nodes": std_dev_color_activated_nodes,
     }
 
     return results
